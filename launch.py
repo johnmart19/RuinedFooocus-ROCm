7fe98a559a162aa185df335374aa3210b6377abe
johnmart19 <johnivan19999@gmail.com>
2026-07-16 07:47:45 +0300

    Update xllamacpp versions for Nvidia
    Currently supported versions:
    * Cuda 13.2
    * Cuda 12.8
    
    Version Changes from 0.2.7 to 2026.7.9873: https://github.com/xorbitsai/xllamacpp/compare/v0.2.7...v2026.7.9873

--- launch.py
+++ launch.py
@@ -138,8 +138,8 @@ def prepare_environment(offline=False):
             xlc_version = "xllamacpp==2026.7.9873"
             if REINSTALL_ALL or not is_installed(xlc_version):
                 platform_index = {
-                    'cu124': 'https://xorbitsai.github.io/xllamacpp/whl/cu124',
                     'cu128': 'https://xorbitsai.github.io/xllamacpp/whl/cu128',
+                    'cu132': 'https://xorbitsai.github.io/xllamacpp/whl/cu132',
                     'rocm6.4': 'https://xorbitsai.github.io/xllamacpp/whl/rocm-6.4.1',
                     'rocm7.2': 'https://xorbitsai.github.io/xllamacpp/whl/rocm-7.2.4',
                     'cpu': 'https://pypi.org/simple'
